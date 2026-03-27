46bff5612f86b63f391d5dfd67de53545bd0c629
vatsprintworks-dot <vatsprintworks@gmail.com>
2026-03-27 21:00:43 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{fontFamily:"sans-serif",padding:"40px"}}>
+      <h1>Vats Printworks</h1>
+      <p>Design. Print. Impress.</p>
+    </div>
+  );
+}
