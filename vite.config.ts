3b62f11ef33b6ba17e69ce6a34d80816264409d1
vatsprintworks-dot <vatsprintworks@gmail.com>
2026-03-27 21:06:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Vats.Printworks/",
-  plugins: [react()]
+base: '/Vats.Printworks/',
+plugins: [react()]
 })
